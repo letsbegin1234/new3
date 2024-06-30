8e652732e0b76dda6fcef55c189b84d9d354b3db
Subhash <98774745+letsbegin1234@users.noreply.github.com>
2024-06-30 17:00:18 +0530

    Update settings.py

--- social_book/settings.py
+++ social_book/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@bnywoh#c*7)ym7vy@zuj$wbckhq8hjtvj=grl*h_c!w@(#4bb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://new3-peok.onrender.com']
 
 
 # Application definition
